43cb8b8fbbeae2f36338e314be82e479f60e3ce7
zyx303 <2091183827@qq.com>
2024-02-16 19:58:11 +0800

    删除一些隐私信息

--- myblog/framework/src/main/java/com/zyx/utils/UploadUtils.java
+++ myblog/framework/src/main/java/com/zyx/utils/UploadUtils.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class UploadUtils {
 
     //阿里域名
-    public static final String UPLOAD_PATH = "https://blogstastic-zyx.oss-cn-hangzhou.aliyuncs.com/";
+    public static final String UPLOAD_PATH = "";
 
     public static String uploadImage(MultipartFile file)
     {
@@ -17,15 +17,15 @@ public class UploadUtils {
         String originalFilename = file.getOriginalFilename();
         String fileName = System.currentTimeMillis() + originalFilename.substring(originalFilename.lastIndexOf("."));
         //地域节点
-        String endpoint = "http://oss-cn-hangzhou.aliyuncs.com";
-        String accessKeyId = "LTAI5t5vYj7fJE2QkeXy7gMX";
-        String accessKeySecret = "BxNeuI1F9T7ja9qAIlOT3a8YZjQVwu";
+        String endpoint = "";
+        String accessKeyId = "";
+        String accessKeySecret = "";
         //oss客户端对象
         OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
         //上传文件流
         try {
             ossClient.putObject(
-                    "blogstastic-zyx", // 仓库名
+                    "", // 仓库名
                     "images/" + fileName, // 文件名
                     file.getInputStream());
         } catch (IOException e) {
