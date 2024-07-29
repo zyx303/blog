121599a2831d79182c597b5a96666befd6f29084
zyx303 <2091183827@qq.com>
2024-07-29 14:45:30 +0800

    Update .gitignore

--- sg-vue-admin/vue.config.js
+++ sg-vue-admin/vue.config.js
@@ -30,7 +30,7 @@ module.exports = {
     proxy: {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        target: `http://47.98.62.200:8989`,
+        target: `http://121.41.83.146:8989`,
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
