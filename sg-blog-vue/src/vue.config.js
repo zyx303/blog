c5f1166e9a6fbf78f339fd73f9dabb4bde55828a
zyx303 <2091183827@qq.com>
2024-08-05 11:22:08 +0800

    删除多余配置

--- sg-blog-vue/src/vue.config.js
+++ sg-blog-vue/src/vue.config.js
@@ -27,16 +27,16 @@ module.exports = {
         host: '0.0.0.0',
         port: port,
         open: true,
-        proxy: {
-            // detail: https://cli.vuejs.org/config/#devserver-proxy
-            [process.env.VUE_APP_BASE_API]: {
-                target: `http://121.41.83.146:7777`,
-                changeOrigin: true,
-                pathRewrite: {
-                    ['^' + process.env.VUE_APP_BASE_API]: ''
-                }
-            }
-        },
+        // proxy: {
+        //     // detail: https://cli.vuejs.org/config/#devserver-proxy
+        //     [process.env.VUE_APP_BASE_API]: {
+        //         target: `http://121.41.83.146:7777`,
+        //         changeOrigin: true,
+        //         pathRewrite: {
+        //             ['^' + process.env.VUE_APP_BASE_API]: ''
+        //         }
+        //     }
+        // },
         disableHostCheck: true
     },
     configureWebpack: {
